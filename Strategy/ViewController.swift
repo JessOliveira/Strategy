36f997371b46b385c3a474d7fcad525b20990889
Jessica <jessicaoliveira3108@gmail.com>
2015-06-10 14:07:55 -0300

    Initial Commit

---
+++ Strategy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Strategy
+//
+//  Created by Jessica Oliveira on 10/06/15.
+//  Copyright (c) 2015 Jessica Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
