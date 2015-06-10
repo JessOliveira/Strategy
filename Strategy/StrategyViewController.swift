a1798a1a248fb3d6ca47f899b6e36eb69976ae8f
Jessica <jessicaoliveira3108@gmail.com>
2015-06-10 16:34:58 -0300

    programando

--- Strategy/StrategyViewController.swift
+++ Strategy/StrategyViewController.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class StrategyViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageMain: UIImageView!
+    @IBOutlet weak var arrowOpponentImageView: UIImageView!
+    @IBOutlet weak var arrowTeamImageView: UIImageView!
+    @IBOutlet weak var teamView: UIView!
+    @IBOutlet weak var opponentView: UIView!
+    
+    var isPanelExpanded = false;
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.scrollView.minimumZoomScale = 1.0
+        self.scrollView.maximumZoomScale = 2.0
+        self.scrollView.zoomScale = 0.2
+        self.scrollView.clipsToBounds = true
+        self.scrollView.contentSize = self.view.frame.size
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +35,64 @@ class StrategyViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //create tap in icon image
+        let tapIcon = UITapGestureRecognizer(target: self, action: Selector("tapPanel:"))
+            teamView.addGestureRecognizer(tapIcon)
+        
+        //create tap in icon image
+        let tapIconOpponent = UITapGestureRecognizer(target: self, action: Selector("tapPanel:"))
+        opponentView.addGestureRecognizer(tapIcon)
+        
     }
-    */
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+    }
+    
+    //MARK: Delegates
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return self.imageMain
+    }
+    
+    
+    //MARK: Tap
+    func tapPanel(gesture:UITapGestureRecognizer) {
+        
+        let animationFunction = isPanelExpanded ? compressPanel : expandPanel
+        
+        //Animate the panel according to proper state transition
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            animationFunction()
+            }, completion: { (result) -> Void in
+                //Revert panel state
+                self.isPanelExpanded = !self.isPanelExpanded
+        })
+    }
+    
+    // MARK: Animation Functions
+    func expandPanel() {
+        
+//        //Change button to compress state
+//        iconImage.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+//        
+//        //TODO: Animate panel size
+//        self.heightPainel.constant = self.view.frame.height * 0.8
+//        self.labelHeight.constant = self.view.frame.height * 0.7
+//        
+//        self.view.layoutIfNeeded()
+    }
+    
+    func compressPanel() {
+//        //Chnage button to expand state
+//        iconImage.transform = CGAffineTransformMakeRotation(0)
+//        
+//        //TODO: Animate panel size
+//        self.heightPainel.constant = 100
+//        self.labelHeight.constant = 25
+//        
+//        self.view.layoutIfNeeded()
+    }
 
 }
